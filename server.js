14d421fd9648797c8884d9bf888897610744e733
Andreas Wimmer <andreas.wimmer@hs-augsburg.de>
2021-10-07 13:48:37 +0200

    new repo

---
+++ server.js
@@ -0,0 +1,37 @@
+const port = process.env.PORT || 3000;
+
+// importing express library and setting up server on port 3000
+let express = require('express');
+let app = express();
+let server = app.listen(port);
+
+// define public directory as static
+// meaning those are the files available to the client
+app.use(express.static('public'));
+
+console.log("Server is running.");
+
+let socket = require('socket.io');
+let io = socket(server);
+
+// execute newConnection when connection event occurs
+io.sockets.on('connection', newConnection);
+function newConnection(socket) {
+    console.log('new connection ' + socket.id);
+
+    // execute newPrediction when prediction event occurs
+    socket.on('prediction', newPrediction);
+    function newPrediction(data){
+        // emits to all sockets but this
+        socket.broadcast.emit('prediction', data);
+        console.log(data);
+    }
+
+    // execute newTimeout when prediction event occurs
+    socket.on('timeout', newTimeout);
+    function newTimeout(data){
+        // emits to all sockets but this
+        socket.broadcast.emit('timeout', data);
+        console.log('timeout');
+    }
+}
